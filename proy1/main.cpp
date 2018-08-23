528f9c2b67c8074719936cd1747a02d8bb2fd661
DOCENTE-N415 <DOCENTE-N415@UCSP.AULAB>
2018-08-23 09:48:44 -0500

    adicion dos numeros

--- proy1/main.cpp
+++ proy1/main.cpp
@@ -4,6 +4,10 @@ using namespace std;
 
 int main()
 {
-    cout << "Hello ALVARITO!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!" << endl;
+    int a = 32766;
+    int b = a + 1;
+    cout << b << endl;
+    b = b + 1;
+    cout << b << endl;
     return 0;
 }
